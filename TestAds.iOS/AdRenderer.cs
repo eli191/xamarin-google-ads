0441a3f5f62f975c94f14064c815776fb3576b30
Elisabeth Mitton <mitton.elisabeth@outlook.com>
2019-04-23 09:52:57 +0200

    iOS Ads

---
+++ TestAds.iOS/AdRenderer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using CoreGraphics;
+using TestAds.iOS.Renderers;
+using Google.MobileAds;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using TestAds.Controls;
+
+[assembly: ExportRenderer(typeof(AdBanner), typeof(AdRenderer))]
+namespace TestAds.iOS.Renderers
+{
+    public class AdRenderer : ViewRenderer<AdBanner, BannerView>
+    {
+        readonly string bannerId = //"ca-app-pub-3940256099942544/2934735716"; //dummy ios ad unit
+            "YOUR AD UNIT ID HERE";
+        BannerView adView;
+        BannerView CreateNativeAdControl()
+        {
+            if (adView != null)
+                return adView;
+
+
+            // Setup your BannerView, review AdSizeCons class for more Ad sizes. 
+            adView = new BannerView(size: AdSizeCons.SmartBannerPortrait,
+                                           origin: new CGPoint(0, UIScreen.MainScreen.Bounds.Size.Height - AdSizeCons.Banner.Size.Height))
+            {
+                AdUnitID = bannerId,
+                RootViewController = GetVisibleViewController()
+            };
+
+            // Wire AdReceived event to know when the Ad is ready to be displayed
+            adView.AdReceived += (object sender, EventArgs e) =>
+            {
+                //ad has come in
+            };
+
+
+            adView.LoadRequest(GetRequest());
+            return adView;
+        }
+
+        Request GetRequest()
+        {
+            var request = Request.GetDefaultRequest();
+            // Requests test ads on devices you specify. Your test device ID is printed to the console when
+            // an ad request is made. GADBannerView automatically returns test ads when running on a
+            // simulator. After you get your device ID, add it here
+            //request.TestDevices = new [] { Request.SimulatorId.ToString () };
+            return request;
+        }
+
+        /// 
+        /// Gets the visible view controller.
+        /// 
+        /// The visible view controller.
+        UIViewController GetVisibleViewController()
+        {
+            var rootController = UIApplication.SharedApplication.KeyWindow?.RootViewController;
+
+            if (rootController == null)
+            {
+                rootController = UIApplication.SharedApplication.Delegate.GetWindow()?.RootViewController;
+            }
+
+            if (rootController.PresentedViewController == null)
+                return rootController;
+
+            if (rootController.PresentedViewController is UINavigationController)
+            {
+                return ((UINavigationController)rootController.PresentedViewController).VisibleViewController;
+            }
+
+            if (rootController.PresentedViewController is UITabBarController)
+            {
+                return ((UITabBarController)rootController.PresentedViewController).SelectedViewController;
+            }
+
+            return rootController.PresentedViewController;
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<AdBanner> e)
+        {
+            base.OnElementChanged(e);
+            if (Control == null)
+            {
+                CreateNativeAdControl();
+                SetNativeControl(adView);
+
+
+            }
+        }
+    }
+}
--- TestAds.iOS/AppDelegate.cs
+++ TestAds.iOS/AppDelegate.cs
@@ -22,6 +22,8 @@ namespace TestAds.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
+            Google.MobileAds.MobileAds.Configure("YOUR APP ID HERE");
+
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
